acc99b81fb29efb6b19d39febb4c3a470d7d1729
Marco Lewandowski <lewandowski@swms.de>
2021-03-15 20:01:40 +0100

    ssl support

--- frontend/src/websockets/config.js
+++ frontend/src/websockets/config.js
@@ -1,6 +1,6 @@
 export default {
 	// TODO: make configurable
-	url: `${getProtocol()}://${window.location.hostname}:${window.location.port}`
+	url: `wss://${window.location.hostname}:${window.location.port}`
 	// url: 'ws://192.168.178.52:8088'
 };
 
